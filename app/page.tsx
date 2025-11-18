e6bd2404f349f17b5c97e6c816c58bceaafaafe2
Nikhil-kesarwani111 <nikhilkesarwani354@gmail.com>
2025-11-18 15:10:01 +0530

    Complete PDF CRUD backend + Alembic migrations

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-// app/page.tsx
-import Layout from '../components/layout'
-import TodoList from '../components/todo-list'
+import Head from 'next/head'
+import Layout from '../components/layout';
+import PDFList from '../components/pdf-list';
+import styles from '../styles/layout.module.css'
 
-export default function HomePage() {
+export default function Home() {
   return (
-    <Layout>
-      <TodoList />
-    </Layout>
+    <div>
+      <Head>
+        <title>Basic PDF CRUD App</title>
+        <meta name="description" content="Basic PDF CRUD App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <PDFList />
+      </Layout>
+    </div>
   )
 }
